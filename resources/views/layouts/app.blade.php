5c441ce6ee7aaa08132df744510ad16836c00188
B0013632 <b00136723@mytudublin.ie>
2023-05-16 16:50:40 +0100

    updated to bootstrap 5 and fixed navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
 		<link rel="icon" href="https://vle-bn.tudublin.ie/pluginfile.php/1/theme_adaptable/favicon/1683025199/faviconTU.ico">
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'> 
         <!-- Bootstrap 3.3.7 --> 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
-        <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
 		<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
@@ -16,18 +16,34 @@
 
     </head> 
 <body>
-  <nav class="navbar navbar-default navbar-static-top">
-    <ul class="nav navbar-nav">
-	  <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="{{ route('employees.index') }}">Employees</a></li>
-      <li><a href="{{ route('customers.index') }}">Customers</a></li>
-      <li><a href="{{ route('orders.index') }}">Orders</a></li>
-      <li><a href="{{ route('reservations.index') }}">Reservations</a></li>
-      <li><a href="{{ route('tables.index') }}">Tables</a></li>
-	  <li><a href="{{ route('calendar.display') }}">Reservations Calendar</a></li>
-    </ul>
-	@include('layouts.navAuth')
-  </nav>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('home') }}">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('employees.index') }}">Employees</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('customers.index') }}">Customers</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('orders.index') }}">Orders</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('reservations.index') }}">Reservations</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('tables.index') }}">Tables</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"] href="{{ route('calendar.display') }}">Reservations Calendar</a>
+			</li>
+		</ul>
+			@include('layouts.navAuth')
+	</div>
+ </nav>
         <div id="page-content-wrapper"> 
             <div class="container-fluid"> 
                 <div class="row"> 
@@ -39,3 +55,12 @@
          </div> 
     </body> 
 </html>
+
+
+
+
+
+
+
+
+ 
--- resources/views/layouts/navAuth.blade.php
+++ resources/views/layouts/navAuth.blade.php
@@ -1,26 +1,21 @@
-<ul class="nav navbar-nav pull-right"> 
-	@if(Auth::guest())
-		<li>
-			<a href="{{route('register')}}">
-				Register
-				<span class="glyphicon glyphicon-pencil"></span>
-			</a>
-		</li>
-		<li>
-			<a href="{{route('login')}}">
-				Login
-				<span class="glyphicon glyphicon-log-in"></span>
-			</a>
-		</li>
-	@else
-		<li>
-			<form action="{{ route('logout') }}" method="POST">
-			@csrf
-				<button type="submit" class="btn btn-primary" style="padding: 15px;">
-					<span class="glyphicon glyphicon-log-out"></span> Log out
-				</button>
-			</form>
-		</li>
-	@endif
-</ul>
-
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <ul class="navbar-nav ml-auto">
+        @if(Auth::guest())
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">Register <span class="glyphicon glyphicon-pencil"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login <span class="glyphicon glyphicon-log-in"></span></a>
+            </li>
+        @else
+            <li class="nav-item">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-primary" style="padding: 15px;">
+                        <span class="glyphicon glyphicon-log-out"></span> Log out
+                    </button>
+                </form>
+            </li>
+        @endif
+    </ul>
+</nav>
